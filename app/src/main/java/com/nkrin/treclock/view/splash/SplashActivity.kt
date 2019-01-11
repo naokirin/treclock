f6921e81f8efd943a9d547ce82a80bb2a6e6dce1
naokirin <naoki.rin186@gmail.com>
2019-01-12 03:28:57 +0900

    Modify package hierarchy

--- app/src/main/java/com/nkrin/treclock/view/splash/SplashActivity.kt
+++ app/src/main/java/com/nkrin/treclock/view/splash/SplashActivity.kt
@@ -1,7 +1,8 @@
-package com.nkrin.treclock
+package com.nkrin.treclock.view.splash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.nkrin.treclock.R
 
 class SplashActivity : AppCompatActivity() {
 
