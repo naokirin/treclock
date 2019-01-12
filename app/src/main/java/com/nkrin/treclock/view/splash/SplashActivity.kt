70ea417254672ea3bd0761a005d7f2f0a85aac04
naokirin <naoki.rin186@gmail.com>
2019-01-12 11:22:43 +0900

    Add splash icon on splash activity

--- app/src/main/java/com/nkrin/treclock/view/splash/SplashActivity.kt
+++ app/src/main/java/com/nkrin/treclock/view/splash/SplashActivity.kt
@@ -2,12 +2,24 @@ package com.nkrin.treclock.view.splash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.nkrin.treclock.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        showLoading()
+    }
+
+    private fun showLoading() {
+        val animation =
+            AnimationUtils.loadAnimation(
+                applicationContext,
+                R.anim.repeated_blinking_animation
+            )
+        splashIcon.startAnimation(animation)
     }
 }
