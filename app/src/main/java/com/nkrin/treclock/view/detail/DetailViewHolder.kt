588c1dfe57f21b563c66b0cbf4bbc4f5dbdc36ac
naokirin <naoki.rin186@gmail.com>
2019-02-08 22:23:03 +0900

    Modify row layout for action button clicked interaction

--- app/src/main/java/com/nkrin/treclock/view/detail/DetailViewHolder.kt
+++ app/src/main/java/com/nkrin/treclock/view/detail/DetailViewHolder.kt
@@ -2,7 +2,6 @@ package com.nkrin.treclock.view.detail
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageButton
 import android.widget.TextView
 import com.nkrin.treclock.R
 import org.jetbrains.anko.find
@@ -12,5 +11,5 @@ class DetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var titleView: TextView = itemView.find(R.id.title)
     var durationView: TextView = itemView.find(R.id.duration)
-    var actionButton: ImageButton = itemView.find(R.id.action_button)
+    var actionButton: View = itemView.find(R.id.action_button)
 }
--- app/src/main/java/com/nkrin/treclock/view/scheduler/SchedulerViewHolder.kt
+++ app/src/main/java/com/nkrin/treclock/view/scheduler/SchedulerViewHolder.kt
@@ -11,6 +11,6 @@ import org.jetbrains.anko.find
 class SchedulerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var titleView: TextView = itemView.find(R.id.title)
     var detailView: TextView = itemView.find(R.id.detail)
-    var actionButton: ImageButton = itemView.find(R.id.action_button)
+    var actionButton: View = itemView.find(R.id.action_button)
     var playingText: TextView = itemView.find(R.id.playing_text)
 }
